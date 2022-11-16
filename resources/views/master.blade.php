858e9318af1bec9f61ef04672c7753e2f664a0ca
robmaestro <robbielaza@gmail.com>
2022-11-16 15:41:28 +0800

    changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Switz - @yield('title')</title>
-    <link href={{ asset('css/app.css') }} rel="stylesheet" type="text/css">
+    <link href={{ asset('css/robbie.css') }} rel="stylesheet" type="text/css">
+    <link href={{ asset('css/john.css') }} rel="stylesheet" type="text/css">
+    <link href={{ asset('css/mendrix.css') }} rel="stylesheet" type="text/css">
+    <link href={{ asset('css/micaella.css') }} rel="stylesheet" type="text/css">
+    <link href={{ asset('css/doms.css') }} rel="stylesheet" type="text/css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
