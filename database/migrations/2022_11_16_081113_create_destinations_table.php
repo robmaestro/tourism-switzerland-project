def2a1332ea4102741ecf2cc67a9147dbfc2c96a
robmaestro <robbielaza@gmail.com>
2022-11-17 11:02:03 +0800

    Update 2022_11_16_081113_create_destinations_table.php

--- database/migrations/2022_11_16_081113_create_destinations_table.php
+++ database/migrations/2022_11_16_081113_create_destinations_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('dest_name');
             $table->string('dest_description');
-            $table->foreignId('review_id')->nullable()->constrained('reviews');
             $table->timestamps();
         });
     }
